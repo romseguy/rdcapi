149ecd51f5b4e36599d693452682b0f01160a591
romseguy <contact@lebonforum.fr>
2025-06-10 10:27:46 +0200

    11

--- src/pages/api/note.ts
+++ src/pages/api/note.ts
@@ -3,22 +3,35 @@ import nextConnect from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Client } from "pg";
-import format from "pg-format";
 import sql from "@/sql";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
 const handler = nextConnect<NextApiRequest, NextApiResponse>()
   .use(cors())
   .get(async (req, res) => {
+    const prefix = new Date() + " ~ GET /note ~ ";
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+
     try {
-      const note = await sql`SELECT * FROM notes WHERE id=${req.query.id}`;
-      res.json(note[0]);
+      const id = req.query.id;
+      if (!id)
+        throw new Error("Vous devez sélectionner une citation à supprimer");
+
+      await client.connect();
+      const res2 = client.query("SELECT * FROM notes WHERE id=$1", [id]);
+      if (res2.rowCount !== 1)
+        throw new Error("La citation n'a pas été trouvée");
+      await client.end();
+      res.json(res2.row[0]);
     } catch (error) {
-      res.send("n");
+      await client.end();
+      res.send({ error, message: error.message });
     }
   })
   .put(async (req, res) => {
-    const prefix = new Date() + " ~ note.put ~ ";
+    const prefix = new Date() + " ~ PUT /note ~ ";
     const client = new Client({
       connectionString: process.env.DATABASE_URL,
     });
@@ -34,35 +47,38 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>()
       if (!user) throw new Error("Vous devez être identifié");
 
       const note = req.body.note;
-      console.log("🚀 ~ .put ~ note:", note);
-
       if (!note)
         throw new Error("Vous devez sélectionner une citation à modifier");
 
       await client.connect();
-      // const query = format(
-      //   'UPDATE "public"."notes" SET "desc" = \'%s\' WHERE "id" = \'%s\'',
-      //   note.desc,
-      //   note.id,
-      // );
-      // console.log("🚀 ~ .put ~ query:", query);
       const query =
-        'UPDATE "public"."notes" SET "desc" = $1, "desc_en" = $2 WHERE "id" = $3';
-      await client.query(query, [note.desc, note.desc_en || "", note.id]);
-
+        'UPDATE "public"."notes" SET "desc" = $1, "desc_en" = $2 WHERE "id" = $3 RETURNING *';
+      const res2 = await client.query(query, [
+        note.desc,
+        note.desc_en || "",
+        note.id,
+      ]);
+      if (res2.rowCount !== 1)
+        throw new Error("La citation n'a pas pu être modifiée");
       await client.end();
+      //res.send(res2.row[0]);
       res.send("o");
     } catch (error) {
       await client.end();
-      console.log(prefix + "error:", error);
       res.send({ error, message: error.message });
     }
   })
   .delete(async (req, res) => {
-    const prefix = new Date() + " ~ note.delete ~ ";
-    let client;
+    const prefix = new Date() + " ~ DELETE /note ~ ";
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
 
     try {
+      const id = req.query.id;
+      if (!id)
+        throw new Error("Vous devez sélectionner une citation à supprimer");
+
       const supabase = createPagesServerClient({ req, res });
       const {
         data: { user, session },
@@ -72,31 +88,17 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>()
       });
       if (!user) throw new Error("Vous devez être identifié");
 
-      const id = req.query.id;
-      console.log("🚀 ~ .delete ~ id:", id);
-
-      if (!id)
-        throw new Error("Vous devez sélectionner une citation à supprimer");
-
-      client = new Client({
-        connectionString: process.env.DATABASE_URL,
-      });
       await client.connect();
-      const query = format(
-        'DELETE FROM "public"."notes" WHERE "id" = \'%s\' RETURNING *',
-        id,
-      );
-      console.log("🚀 ~ .delete ~ query:", query);
-      const res2 = await client.query(query);
+      const query = 'DELETE FROM "public"."notes" WHERE "id" = $1 RETURNING *';
+      const res2 = await client.query(query, [id]);
       if (res2.rowCount !== 1)
         throw new Error("La citation n'a pas pu être supprimée");
-
-      res.send("o");
+      await client.end();
+      res.send(res2.row[0]);
     } catch (error) {
+      await client.end();
       console.log(prefix + "error:", error);
       res.send({ error, message: error.message });
-    } finally {
-      await client.end();
     }
   });
 
