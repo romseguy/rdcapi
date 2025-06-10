a138358149274c7599e6af1878a0d3e4e6db2913
romseguy <contact@lebonforum.fr>
2025-06-10 10:27:41 +0200

    10

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,20 +7,24 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = nextConnect<NextApiRequest, NextApiResponse>()
   .use(cors())
   .get(async (req, res) => {
+    const prefix = new Date() + " ~ login.get ~ ";
+
     try {
       const supabase = createPagesServerClient({ req, res });
-      const access_token = req.query.at;
-      const refresh_token = req.query.rt;
+      const access_token = req.headers.at as string;
+      const refresh_token = req.headers.rt as string;
       const {
         data: { user, session },
       } = await supabase.auth.setSession({
         access_token,
         refresh_token,
       });
+      if (!user) throw new Error("Cet utilisateur est introuvable");
 
       res.json(user);
     } catch (error) {
-      res.send("n");
+      console.log(prefix + "error:", error);
+      res.send({ error, message: error.message });
     }
   })
   .post(async (req, res) => {
@@ -32,8 +36,12 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>()
       const creds: SignInWithPasswordCredentials = { email, password };
       const resp = await supabase.auth.signInWithPassword(creds);
       if (resp.error) {
-        const respp = await supabase.auth.signUp(creds);
-        if (respp.error) throw respp.error;
+        if (password.length >= 6) {
+          const respp = await supabase.auth.signUp(creds);
+          if (respp.error) throw respp.error;
+        } else {
+          throw new Error("Identifiants incorrects");
+        }
         return res.json(respp.data);
       }
       res.json(resp.data);
